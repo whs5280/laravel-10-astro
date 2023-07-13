62055d584baefb071ce5d3fadb4bcd3612b68964
whs5280 <13434118350@163.com>
2023-07-13 11:12:38 +0800

    feat: 数据建模 - 星座表

---
+++ database/migrations/2023_06_27_072614_create_astros_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('astros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedTinyInteger('index')->index()->comment('索引');
+            $table->integer('date')->comment('日期');
+            $table->string('constellation', 3)->comment('星座');
+
+            $table->string('overall_desc')->nullable()->comment('综合运势');
+            $table->string('romance_desc')->nullable()->comment('爱情运势');
+            $table->string('workjob_desc')->nullable()->comment('事业运势');
+            $table->string('mood_desc')->nullable()->comment('心情运势');
+            $table->string('communication_desc')->nullable()->comment('交际运势');
+            $table->string('wealth_desc')->nullable()->comment('财富运势');
+            $table->string('health_desc')->nullable()->comment('健康运势');
+
+            $table->string('lucky_astro')->nullable()->comment('速配星座');
+            $table->string('alert_astro')->nullable()->comment('提防星座');
+            $table->string('lucky_color')->nullable()->comment('幸运颜色');
+            $table->string('lucky_number')->nullable()->comment('幸运数字');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('astros');
+    }
+};
